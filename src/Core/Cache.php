f96fa406216ce699bf496d57b9a0c2fecbc1712c
jaakon <mvs@albami.de>
2016-02-19 15:24:08 +0100

    changes for new version of stash

--- src/Core/Cache.php
+++ src/Core/Cache.php
@@ -4,24 +4,32 @@ class Cache
 {
 	public static function clear($key)
 	{
-		return App::get('Cache')->getItem($key)->clear();
+		return App::get('Cache')->deleteItem(static::groupKeyFragments($key));
 	}
 
 	public static function flush()
 	{
-		return App::get('Cache')->flush();
+		return App::get('Cache')->clear();
 	}
 
 	public static function get($key, callable $callback = null, $ttl = null)
 	{
 		/** @var \Stash\Item $item */
-		$item  = App::get('Cache')->getItem($key);
+		$item  = App::get('Cache')->getItem(static::groupKeyFragments($key));
 		$value = $item->get();
 
 		if ($item->isMiss() && $callback !== null) {
 			$item->lock();
+
 			$value = $callback();
-			$item->set($value, $ttl);
+
+			$item->set($value);
+
+			if ($ttl !== null) {
+				$item->expiresAfter($ttl);
+			}
+
+			App::get('Cache')->save($item);
 		}
 
 		return $value;
@@ -29,6 +37,20 @@ class Cache
 
 	public static function set($key, $value, $ttl = null)
 	{
-		App::get('Cache')->getItem($key)->set($value, $ttl);
+		/** @var \Stash\Item $item */
+		$item = App::get('Cache')->getItem(static::groupKeyFragments($key));
+
+		$item->set($value);
+
+		if ($ttl !== null) {
+			$item->expiresAfter($ttl);
+		}
+
+		App::get('Cache')->save($item);
+	}
+
+	private static function groupKeyFragments($key)
+	{
+		return is_array($key) ? implode('/', $key) : $key;
 	}
 }
