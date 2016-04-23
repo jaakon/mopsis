829034c6d815938284e6d9940faf3545770d0858
jaakon <mvs@albami.de>
2016-04-23 10:46:44 +0200

    changes needed for package updates

--- src/Core/Cache.php
+++ src/Core/Cache.php
@@ -1,35 +1,63 @@
-<?php namespace Mopsis\Core;
+<?php
+namespace Mopsis\Core;
 
 use Mopsis\Core\App;
 
 class Cache
 {
-	public static function clear($key)
-	{
-		return App::make('Cache')->getItem($key)->clear();
-	}
+    public static function clear($key)
+    {
+        return App::make('Cache')->deleteItem(static::groupKeyFragments($key));
+    }
 
-	public static function flush()
-	{
-		return App::make('Cache')->flush();
-	}
+    public static function flush()
+    {
+        return App::make('Cache')->clear();
+    }
 
-	public static function get($key, callable $callback = null, $ttl = null)
-	{
-		$item  = App::make('Cache')->getItem($key);
-		$value = $item->get();
+    public static function get($key, callable $callback = null, $ttl = null)
+    {
+        /**
+         * @var \Stash\Item $item
+         */
+        $item  = App::make('Cache')->getItem(static::groupKeyFragments($key));
+        $value = $item->get();
 
-		if ($item->isMiss() && $callback !== null) {
-			$item->lock();
-			$value = $callback();
-			$item->set($value, $ttl);
-		}
+        if ($item->isMiss() && $callback !== null) {
+            $item->lock();
 
-		return $value;
-	}
+            $value = $callback();
 
-	public static function set($key, $value, $ttl = null)
-	{
-		App::make('Cache')->getItem($key)->set($value, $ttl);
-	}
+            $item->set($value);
+
+            if ($ttl !== null) {
+                $item->expiresAfter($ttl);
+            }
+
+            App::make('Cache')->save($item);
+        }
+
+        return $value;
+    }
+
+    public static function set($key, $value, $ttl = null)
+    {
+        /**
+         * @var \Stash\Item $item
+         */
+        $item = App::make('Cache')->getItem(static::groupKeyFragments($key));
+
+        $item->set($value);
+
+        if ($ttl !== null) {
+            $item->expiresAfter($ttl);
+        }
+
+        App::make('Cache')->save($item);
+    }
+
+    private static function groupKeyFragments($key)
+    {
+        return is_array($key) ? implode('/', $key) : $key;
+    }
 }
--- src/config.php
+++ src/config.php
@@ -1,278 +1,294 @@
 <?php
 
-use Interop\Container\ContainerInterface as ContainerInterface;
-
 use function DI\get;
 use function DI\object;
+use Interop\Container\ContainerInterface as ContainerInterface;
 
 return [
-	'flysystem.local.config' => 'storage/files',
-	'monolog.lineformat'     => "[%datetime%] %level_name%: %message% %context% %extra%\n",
-	'namespacedModels'       => '\\App\\{{MODUL}}\\Domain\\{{DOMAIN}}Model',
-	'stash.apc.config'       => [
-		'ttl'       => 3600,
-		'namespace' => md5($_SERVER['HTTP_HOST'])
-	],
-	'stash.redis.config' => [
-		'servers' => [['server' => '127.0.0.1', 'port' => '6379', 'ttl' => 10]]
-	],
-	'stash.sqlite.config'    => [
-		'path' => 'storage/cache/'
-	],
-	'translator.locale'      => 'de',
-	'translator.path'        => 'resources/lang/',
-	'twig.dev.config'        => [
-		'debug'            => true,
-		'cache'            => false,
-		'auto_reload'      => true,
-		'strict_variables' => true
-	],
-	'twig.live.config'       => [
-		'debug'            => false,
-		'cache'            => 'storage/cache/',
-		'auto_reload'      => false,
-		'strict_variables' => false
-	],
-	'twig.config'            => get('twig.dev.config'),
-	'twigloader.config'      => ['app/views'],
+    'flysystem.local.config' => 'storage/files',
+    'monolog.lineformat'     => "[%datetime%] %level_name%: %message% %context% %extra%\n",
+    'namespacedModels'       => '\\App\\{{MODUL}}\\Domain\\{{DOMAIN}}Model',
+    'stash.apc.config'       => [
+        'ttl'       => 3600,
+        'namespace' => md5($_SERVER['HTTP_HOST'])
+    ],
+    'stash.redis.config'     => [
+        'servers' => [['server' => '127.0.0.1', 'port' => '6379', 'ttl' => 10]]
+    ],
+    'stash.sqlite.config'    => [
+        'path' => 'storage/cache/'
+    ],
+    'translator.locale'      => 'de',
+    'translator.path'        => 'resources/lang/',
+    'twig.dev.config'        => [
+        'debug'            => true,
+        'cache'            => false,
+        'auto_reload'      => true,
+        'strict_variables' => true
+    ],
+    'twig.live.config'       => [
+        'debug'            => false,
+        'cache'            => 'storage/cache/',
+        'auto_reload'      => false,
+        'strict_variables' => false
+    ],
+    'twig.config'            => get('twig.dev.config'),
+    'twigloader.config'      => ['app/views'],
 
-	Aptoma\Twig\Extension\MarkdownEngineInterface::class
-		=> object(Mopsis\Extensions\Twig\Markdown\MarkdownEngine::class),
+    Aptoma\Twig\Extension\MarkdownEngineInterface::class
+    => object(Mopsis\Extensions\Twig\Markdown\MarkdownEngine::class),
 
-	Asm89\Twig\CacheExtension\CacheProviderInterface::class
-		=> object(Mopsis\Extensions\Twig\Cache\CacheAdapter::class),
+    Asm89\Twig\CacheExtension\CacheProviderInterface::class
+    => object(Mopsis\Extensions\Twig\Cache\CacheAdapter::class),
 
-	Asm89\Twig\CacheExtension\CacheStrategyInterface::class
-		=> object(Asm89\Twig\CacheExtension\CacheStrategy\GenerationalCacheStrategy::class),
+    Asm89\Twig\CacheExtension\CacheStrategyInterface::class
+    => object(Asm89\Twig\CacheExtension\CacheStrategy\GenerationalCacheStrategy::class),
 
-	Asm89\Twig\CacheExtension\CacheStrategy\KeyGeneratorInterface::class
-		=> object(Mopsis\Extensions\Twig\Cache\KeyGenerator::class),
+    Asm89\Twig\CacheExtension\CacheStrategy\KeyGeneratorInterface::class
+    => object(Mopsis\Extensions\Twig\Cache\KeyGenerator::class),
 
-	Aura\Filter\FilterFactory::class
-		=> object()
-		->constructorParameter('validate_factories', [
-			'bic' => function () {
-				return app(Mopsis\Extensions\Aura\Filter\Rule\Validate\Bic::class);
-			},
-			'iban' => function () {
-				return app(Mopsis\Extensions\Aura\Filter\Rule\Validate\Iban::class);
-			},
-			'money' => function () {
-				return app(Mopsis\Extensions\Aura\Filter\Rule\Validate\Money::class);
-			},
-			'optional' => function () {
-				return app(Mopsis\Extensions\Aura\Filter\Rule\Validate\Optional::class);
-			},
-			'zipcode' => function () {
-				return app(Mopsis\Extensions\Aura\Filter\Rule\Validate\ZipCode::class);
-			}
-		])
-		->constructorParameter('sanitize_factories', [
-			'float' => function () {
-				return app(Mopsis\Extensions\Aura\Filter\Rule\Sanitize\Float::class);
-			}
-		]),
+    Aura\Filter\FilterFactory::class
+    => object()
+        ->constructorParameter('validate_factories', [
+            'bic'      => function () {
+                return app(Mopsis\Extensions\Aura\Filter\Rule\Validate\Bic::class);
+            },
+            'iban'     => function () {
+                return app(Mopsis\Extensions\Aura\Filter\Rule\Validate\Iban::class);
+            },
+            'money'    => function () {
+                return app(Mopsis\Extensions\Aura\Filter\Rule\Validate\Money::class);
+            },
+            'optional' => function () {
+                return app(Mopsis\Extensions\Aura\Filter\Rule\Validate\Optional::class);
+            },
+            'zipcode'  => function () {
+                return app(Mopsis\Extensions\Aura\Filter\Rule\Validate\ZipCode::class);
+            }
 
-	Aura\Filter\SubjectFilter::class
-		=> function (ContainerInterface $c) {
-			return $c->get(Aura\Filter\FilterFactory::class)->newSubjectFilter();
-		},
+        ])
+        ->constructorParameter('sanitize_factories', [
+            'float' => function () {
+                return app(Mopsis\Extensions\Aura\Filter\Rule\Sanitize\Float::class);
+            }
 
-	Aura\Web\Request::class
-		=> function (ContainerInterface $c) {
-			return $c->get(Aura\Web\WebFactory::class)->newRequest();
-		},
+        ]),
 
-	Aura\Web\Response::class
-		=> function (ContainerInterface $c) {
-			return $c->get(Aura\Web\WebFactory::class)->newResponse();
-		},
+    Aura\Filter\SubjectFilter::class
+    => function (ContainerInterface $c) {
+        return $c->get(Aura\Filter\FilterFactory::class)->newSubjectFilter();
+    },
 
-	Aura\Web\WebFactory::class
-		=> function () {
-			if (count($_POST) && !count($_FILES)) {
-				// php://input is not available with enctype="multipart/form-data"
-				// perhaps "enable_post_data_reading = off" can help?
+    Aura\Web\Request::class
+    => function (ContainerInterface $c) {
+        return $c->get(Aura\Web\WebFactory::class)->newRequest();
+    },
 
-				$_POST = [];
+    Aura\Web\Response::class
+    => function (ContainerInterface $c) {
+        return $c->get(Aura\Web\WebFactory::class)->newResponse();
+    },
 
-				foreach (explode('&', file_get_contents('php://input')) as $entry) {
-					list($key,) = array_map('urldecode', explode('=', $entry));
-					$key = preg_replace('/\[(.*)\]$/', '', $key);
-					$_POST[$key] = $_REQUEST[str_replace(['.', ' '], '_', $key)];
+    Aura\Web\WebFactory::class
+    => function () {
+        if (count($_POST) && !count($_FILES)) {
+// php://input is not available with enctype="multipart/form-data"
+            // perhaps "enable_post_data_reading = off" can help?
 
-					if (!is_array($_POST[$key])) {
-						$_POST[$key] = trim($_POST[$key]);
-					}
-				}
-			}
+            $_POST = [];
 
-			return new Aura\Web\WebFactory([
-				'_COOKIE' => $_COOKIE,
-				'_ENV'    => $_ENV,
-				'_FILES'  => $_FILES,
-				'_POST'   => $_POST,
-				'_GET'    => $_GET,
-				'_SERVER' => $_SERVER
-			]);
-		},
+            foreach (explode('&', file_get_contents('php://input')) as $entry) {
+                list($key)   = array_map('urldecode', explode('=', $entry));
+                $key         = preg_replace('/\[(.*)\]$/', '', $key);
+                $_POST[$key] = $_REQUEST[str_replace(['.', ' '], '_', $key)];
 
-	Illuminate\Translation\LoaderInterface::class
-		=> object(Illuminate\Translation\FileLoader::class)
-		->constructorParameter('path', get('translator.path')),
+                if (!is_array($_POST[$key])) {
+                    $_POST[$key] = trim($_POST[$key]);
+                }
+            }
+        }
 
-	League\Flysystem\AdapterInterface::class
-		=> object(League\Flysystem\Adapter\Local::class)
-		->constructor(get('flysystem.local.config')),
+        return new Aura\Web\WebFactory([
+            '_COOKIE' => $_COOKIE,
+            '_ENV'    => $_ENV,
+            '_FILES'  => $_FILES,
+            '_POST'   => $_POST,
+            '_GET'    => $_GET,
+            '_SERVER' => $_SERVER
+        ]);
+    },
 
-	League\Flysystem\CacheInterface::class
-		=> object(League\Flysystem\Cache\Stash::class)
-		->constructor(get('Cache')),
+    Illuminate\Translation\LoaderInterface::class
+    => object(Illuminate\Translation\FileLoader::class)
+        ->constructorParameter('path', get('translator.path')),
 
-	Monolog\Formatter\LineFormatter::class
-		=> object()
-		->constructorParameter('format', get('monolog.lineformat'))
-		->constructorParameter('allowInlineLineBreaks', true),
+    League\Flysystem\AdapterInterface::class
+    => object(League\Flysystem\Adapter\Local::class)
+        ->constructor(get('flysystem.local.config')),
 
-	Mopsis\Core\User::class
-		=> function () {
-			return Mopsis\Core\Auth::user();
-		},
+    League\Flysystem\CacheInterface::class
+    => object(League\Flysystem\Cache\Stash::class)
+        ->constructor(get('Cache')),
 
-	Mopsis\Components\View\View::class
-		=> function (ContainerInterface $c) {
-			$extensions = [
-				$c->get(Asm89\Twig\CacheExtension\Extension::class),
-				$c->get(Mopsis\Extensions\Twig\FormBuilder::class),
-				$c->get(Mopsis\Extensions\Twig\Markdown::class)
-			];
+    Monolog\Formatter\LineFormatter::class
+    => object()
+        ->constructorParameter('format', get('monolog.lineformat'))
+        ->constructorParameter('allowInlineLineBreaks', true),
 
-			if ($c->get('twig.config')['debug']) {
-				$extensions[] = $c->get(Twig_Extension_Debug::class);
-			}
+    Mopsis\Core\User::class
+    => function () {
+        return Mopsis\Core\Auth::user();
+    },
 
-			return new Mopsis\Components\View\View($c->get(Twig_Environment::class), $extensions);
-		},
+    Mopsis\Components\View\View::class
+    => function (ContainerInterface $c) {
+        $extensions = [
+            $c->get(Asm89\Twig\CacheExtension\Extension::class),
+            $c->get(Mopsis\Extensions\Twig\FormBuilder::class),
+            $c->get(Mopsis\Extensions\Twig\Markdown::class)
+        ];
 
-	Psr\Log\LoggerInterface::class
-		=> get('Logger'),
+        if ($c->get('twig.config')['debug']) {
+            $extensions[] = $c->get(Twig_Extension_Debug::class);
+        }
 
-	Stash\Interfaces\PoolInterface::class
-		=> get('Cache'),
+        return new Mopsis\Components\View\View($c->get(Twig_Environment::class), $extensions);
+    },
 
-	Stash\Driver\Apc::class
-		=> object()
-		->method('setOptions', get('stash.apc.config')),
+    Psr\Log\LoggerInterface::class
+    => get('Logger'),
 
-	Stash\Driver\Redis::class
-		=> object()
-		->method('setOptions', get('stash.redis.config')),
+    Stash\Interfaces\PoolInterface::class
+    => get('Cache'),
 
-	Stash\Driver\Sqlite::class
-		=> object()
-		->method('setOptions', get('stash.sqlite.config')),
+    Stash\Driver\Apc::class
+    => object()
+        ->constructor(get('stash.apc.config')),
 
-	Twig_LoaderInterface::class
-		=> object(Twig_Loader_Filesystem::class)
-		->constructor(get('twigloader.config')),
+    Stash\Driver\Redis::class
+    => object()
+        ->constructor(get('stash.redis.config')),
 
-	Twig_Environment::class
-		=> object()
-		->constructor(get(Twig_LoaderInterface::class), get('twig.config')),
+    Stash\Driver\Sqlite::class
+    => object()
+        ->constructor(get('stash.sqlite.config')),
 
-	Whoops\Handler\JsonResponseHandler::class
-		=> object()
-		->method('addTraceToOutput', true)
-		->method('onlyForAjaxRequests', true),
+    Twig_LoaderInterface::class
+    => object(Twig_Loader_Filesystem::class)
+        ->constructor(get('twigloader.config')),
 
-	Whoops\Handler\PlainTextHandler::class
-		=> object()
-		->constructor(get('Logger')),
+    Twig_Environment::class
+    => object()
+        ->constructor(get(Twig_LoaderInterface::class), get('twig.config')),
 
-	Whoops\Handler\PrettyPageHandler::class
-		=> object()
-		->method('setEditor', 'sublime'),
+    Whoops\Handler\JsonResponseHandler::class
+    => object()
+        ->method('addTraceToOutput', true)
+        ->method('onlyForAjaxRequests', true),
 
-	Cache::class
-		=> object(Stash\Pool::class)
-		->constructor(get('StashDriver'))
-		->method('setNamespace', md5($_SERVER['HTTP_HOST'])),
+    Whoops\Handler\PlainTextHandler::class
+    => object()
+        ->constructor(get('Logger')),
 
-	CacheTool::class
-		=> function () {
-			$adapter = new CacheTool\Adapter\FastCGI('127.0.0.1:9000');
+    Whoops\Handler\PrettyPageHandler::class
+    => object()
+        ->method('setEditor', 'sublime'),
 
-			return CacheTool\CacheTool::factory($adapter);
-		},
+    Cache::class
+    => object(Stash\Pool::class)
+        ->constructor(get('StashDriver'))
+        ->method('setNamespace', md5($_SERVER['HTTP_HOST'])),
 
-	Database::class
-		=> function () {
-			$manager = new Illuminate\Database\Capsule\Manager;
-			$manager->addConnection([
-				'driver'    => SQL_DRIVER,
-				'host'      => SQL_HOST,
-				'database'  => SQL_DATABASE,
-				'username'  => SQL_USERNAME,
-				'password'  => SQL_PASSWORD,
-				'charset'   => 'utf8',
-				'collation' => 'utf8_general_ci',
-				'prefix'    => ''
-			]);
+    CacheTool::class
+    => function () {
+        $adapter = new CacheTool\Adapter\FastCGI('127.0.0.1:9000');
 
-			$manager->setEventDispatcher(new Illuminate\Events\Dispatcher);
-			$manager->bootEloquent();
-			$manager->setAsGlobal();
+        return CacheTool\CacheTool::factory($adapter);
+    },
 
-			return $manager;
-		},
+    Database::class
+    => function () {
+        $manager = new Illuminate\Database\Capsule\Manager();
+        $manager->addConnection([
+            'driver'    => SQL_DRIVER,
+            'host'      => SQL_HOST,
+            'database'  => SQL_DATABASE,
+            'username'  => SQL_USERNAME,
+            'password'  => SQL_PASSWORD,
+            'charset'   => 'utf8',
+            'collation' => 'utf8_general_ci',
+            'prefix'    => ''
+        ]);
 
-	ErrorHandler::class
-		=> function (ContainerInterface $c) {
-			$whoops = new Whoops\Run;
+        $manager->setEventDispatcher(new Illuminate\Events\Dispatcher());
+        $manager->bootEloquent();
+        $manager->setAsGlobal();
 
-			$whoops->pushHandler($c->get(Whoops\Handler\PrettyPageHandler::class));
-			$whoops->pushHandler($c->get(Whoops\Handler\PlainTextHandler::class));
-			$whoops->pushHandler($c->get(Whoops\Handler\JsonResponseHandler::class));
-			$whoops->pushHandler(function ($exception, $inspector, $run) use ($c) {
-				$c->get(Logger::class)->error($exception->getMessage());
-			});
+        return $manager;
+    },
 
-			$whoops->register();
+    ErrorHandler::class
+    => function (ContainerInterface $c) {
+        $whoops = new Whoops\Run();
 
-			return $whoops;
-		},
+        $whoops->pushHandler($c->get(Whoops\Handler\PrettyPageHandler::class));
+        $whoops->pushHandler($c->get(Whoops\Handler\PlainTextHandler::class));
+        $whoops->pushHandler($c->get(Whoops\Handler\JsonResponseHandler::class));
+        $whoops->pushHandler(function ($exception, $inspector, $run) use ($c) {
+            $c->get(Logger::class)->error($exception->getMessage());
+        });
 
-	Filesystem::class
-		=> object(League\Flysystem\Filesystem::class),
+        $whoops->register();
 
-	Flash::class
-		=> object(Mopsis\Extensions\Flash::class),
+        return $whoops;
+    },
 
-	Logger::class
-		=> function (ContainerInterface $c) {
-			$errorHandler = new Monolog\Handler\StreamHandler(CORE_ERROR_LOG, Monolog\Logger::ERROR, false);
-			$errorHandler->setFormatter($c->get(Monolog\Formatter\LineFormatter::class));
+    Filesystem::class
+    => object(League\Flysystem\Filesystem::class),
 
-			$noticeHandler = new Monolog\Handler\StreamHandler(CORE_APPLICATION_LOG, Monolog\Logger::NOTICE, false);
-			$noticeHandler->setFormatter($c->get(Monolog\Formatter\LineFormatter::class));
+    Flash::class
+    => object(Mopsis\Extensions\Flash::class),
 
-			$logger = new Monolog\Logger('default');
+    Logger::class
+    => function (ContainerInterface $c) {
+        $errorHandler = new Monolog\Handler\StreamHandler(CORE_ERROR_LOG, Monolog\Logger::ERROR, false);
+        $errorHandler->setFormatter($c->get(Monolog\Formatter\LineFormatter::class));
 
-			$logger->pushHandler(new Monolog\Handler\ChromePHPHandler(Monolog\Logger::DEBUG));
-			$logger->pushHandler($noticeHandler);
-			$logger->pushHandler($errorHandler);
-			$logger->pushHandler(new Monolog\Handler\PushoverHandler('aw6zvva5hvy67Y1gvnagx7y3GZzEDA', 'uF1VyiRtDd1XXnEKA41imF2P88gxJ4', PROJECT_TITLE, Monolog\Logger::ERROR, false));
+        $noticeHandler = new Monolog\Handler\StreamHandler(CORE_APPLICATION_LOG, Monolog\Logger::NOTICE, false);
+        $noticeHandler->setFormatter($c->get(Monolog\Formatter\LineFormatter::class));
 
-			return $logger;
-		},
+        $logger = new Monolog\Logger('default');
 
-	Renderer::class
-		=> object(Twig_Environment::class),
+        $logger->pushHandler(new Monolog\Handler\ChromePHPHandler(Monolog\Logger::DEBUG));
+        $logger->pushHandler($noticeHandler);
+        $logger->pushHandler($errorHandler);
+        $logger->pushHandler(new Monolog\Handler\PushoverHandler('aw6zvva5hvy67Y1gvnagx7y3GZzEDA', 'uF1VyiRtDd1XXnEKA41imF2P88gxJ4', PROJECT_TITLE, Monolog\Logger::ERROR, false));
 
-	StashDriver::class
-		=> object(Stash\Driver\Redis::class),
+        return $logger;
+    },
 
-	Translator::class
-		=> object(Illuminate\Translation\Translator::class)
-		->constructorParameter('locale', get('translator.locale'))
+    Renderer::class
+    => object(Twig_Environment::class),
+
+    StashDriver::class
+    => object(
+        array_shift(
+            array_filter(
+                [
+                    'redis'    => \Stash\Driver\Redis::class,
+                    'apcu'     => \Stash\Driver\Apc::class,
+                    'sqlite3'  => \Stash\Driver\Sqlite::class,
+                    'standard' => \Stash\Driver\FileSystem::class
+                ],
+                function ($name) {
+                    return extension_loaded($name);
+                },
+                ARRAY_FILTER_USE_KEY
+            )
+        )
+    ),
+
+    Translator::class
+    => object(Illuminate\Translation\Translator::class)
+        ->constructorParameter('locale', get('translator.locale'))
 ];
